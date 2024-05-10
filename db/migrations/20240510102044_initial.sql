167fb25078b53561a717dc572979c99cefe2a72e
Daniil Kikimov <katsushooter@gmail.com>
2024-05-10 16:43:05 +0300

    feat: update migrations

--- db/migrations/20240510102044_initial.sql
+++ db/migrations/20240510102044_initial.sql
@@ -1,23 +1,21 @@
 -- +goose Up
 -- +goose StatementBegin
-create table files (
+create table file_entities (
     id uint64 primary key unique,
     parent_id uint64 not null references files(id) on delete cascade,
     name text not null,
-    size int not null,
-    message_id int not null,
-    file_id text not null,
-    file_size uint32 not null,
-    created_at timestamp not null default current_timestamp,
-    updated_at timestamp not null default current_timestamp
+    message_id int,
+    file_id text,
+    file_size uint32,
+    created_at datetime not null default current_timestamp,
+    updated_at datetime not null default current_timestamp
 );
 
-create index files_parent_name on files(parent_id, name);
+create index entity_parent_name on file_entities(parent_id, name);
 
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 drop table files;
-drop index files_parent_name;
 -- +goose StatementEnd
