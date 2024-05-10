5e5e4dd162d21eeb83e9a4af9360e8d24e23782e
Daniil Kikimov <katsushooter@gmail.com>
2024-05-10 12:11:04 +0300

    feat: add sqlite baseline

--- internal/telegram/telegram.go
+++ internal/telegram/telegram.go
@@ -1,1 +1,11 @@
 package telegram
+
+import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+type Bot struct {
+	api *tgbotapi.BotAPI
+}
+
+func NewBot(api *tgbotapi.BotAPI) *Bot {
+	return &Bot{api: api}
+}
